fb9f1dbc5723628ccae173762e8970017d7b7132
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2015-12-10 22:03:30 +0200

    Added the security check at start. File edit java menu bars. and some menuBarItems but with no functionality

--- src/Document.java
+++ src/Document.java
@@ -5,59 +5,121 @@ import java.awt.*;
 public class Document extends JFrame {
     private JTextArea textArea;
     private JMenuBar menuBar;
-    private JMenu menuFile, menuEdit, menuCompile;
+    private JMenu menuFile, menuEdit, menuJava;
     private JScrollPane scrollPane;
-    private JMenuItem copyMenuItem, pasteMenuItem, saveMenuItem;
+    private String documentName ;
+    private JMenuItem copyMenuItem, pasteMenuItem, saveMenuItem, saveAsMenuItem, compileFileMenuItem, compileSelectedMenuitem,
+            runFileMenuItem, runSelectedMenuItem, newFileMenuItem;
+
     private JToolBar toolBar;
-    public Document(){
-        super("Document");
+
+
+    public Document(String documentName){
+        super(documentName);
+
+
         setSize(800,800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         Container pane = getContentPane();
         pane.setLayout(new BorderLayout());
-
         toolBar = new JToolBar();
-
         textArea = new JTextArea();
-        textArea.setBackground(Color.gray);
-        textArea.setSelectedTextColor(Color.blue);
-        
-
-
-        menuFile = new JMenu("File"); //file menu
-        menuEdit = new JMenu("Edit"); //edit menu
-        menuCompile = new JMenu("Compile"); //edit menu
+        setTextAreaProperties();
         scrollPane = new JScrollPane(textArea);
-
-        menuBar = new JMenuBar();
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        setJMenuBar(menuBar);
-
         pane.add(scrollPane,BorderLayout.CENTER);
         pane.add(toolBar,BorderLayout.SOUTH);
-
-
-        menuBar.add(menuFile);
-        menuBar.add(menuEdit);
-        menuBar.add(menuCompile);
-
-
-
-
-
-
-
-        //  menuFile.add(saveMenuItem);
-
         setVisible(true);
+
+
+        initializeTheMenuBar();
+        setUpMenus();
+        addingTheMenus();
+        settingUpTheMenuItems();
+        addingTheMenuItems();
+
 
 
     }
 
 
 
+
+    private void setTextAreaProperties(){
+
+        textArea.setBackground(Color.gray);
+        textArea.setSelectedTextColor(Color.blue);
+
+        Font font = new Font("Verdana", Font.PLAIN, 14);
+        textArea.setFont(font);
+        textArea.setForeground(Color.white);
+
+    }
+
+    private void setUpMenus(){
+        //setting up  menus
+        menuFile = new JMenu("File"); //file menu
+        menuEdit = new JMenu("Edit"); //edit menu
+        menuJava = new JMenu("Java"); //java menu
+
+
+    }
+
+    private void addingTheMenus(){
+        //adding the menus
+        menuBar.add(menuFile);
+        menuBar.add(menuEdit);
+        menuBar.add(menuJava);
+
+
+    }
+
+    private void settingUpTheMenuItems(){
+
+        newFileMenuItem = new JMenuItem("New File");
+        saveMenuItem = new JMenuItem("Save");
+        saveAsMenuItem = new JMenuItem("Save as");
+        pasteMenuItem = new JMenuItem("Paste");
+        copyMenuItem = new JMenuItem("Copy");
+        compileFileMenuItem = new JMenuItem("Compile File");
+        compileSelectedMenuitem = new JMenuItem("Compile Selected");
+        runFileMenuItem = new JMenuItem("Run File");
+        runSelectedMenuItem = new JMenuItem("Run Selected");
+
+
+
+    }
+
+    private void addingTheMenuItems(){
+
+        //adding the menu items
+        menuFile.add(newFileMenuItem);
+        menuFile.add(saveMenuItem);
+        menuFile.add(saveAsMenuItem);
+        menuEdit.add(copyMenuItem);
+        menuEdit.add(pasteMenuItem);
+        menuJava.add(compileFileMenuItem);
+        menuJava.add(compileSelectedMenuitem);
+        menuJava.add(runFileMenuItem);
+        menuJava.add(runSelectedMenuItem);
+
+
+
+
+
+
+    }
+
+    private void initializeTheMenuBar(){
+        //initializing the menu bar
+        menuBar = new JMenuBar();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        setJMenuBar(menuBar);
+    }
+
+
+
+    
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import com.sun.codemodel.internal.JOp;
+
+import javax.swing.*;
+
 public class Main{
 
 
     public static void main(String[] args) {
-        System.out.print("hell");
-        Document doc = new Document();
+//
+//        String secretCode ;
+//
+//        do {
+//             secretCode =  JOptionPane.showInputDialog(null, "Hello Wassim\nEnter your password ", "Access Code required", JOptionPane.INFORMATION_MESSAGE);
+//
+//
+//              System.out.println(secretCode.toString());
+//
+//            if (!secretCode.equals("abc")) {
+//                JOptionPane.showMessageDialog(null,"Wrong password");
+//            }
+//        }while (!secretCode.equals("abc"));
+
+        String documentName = (String) JOptionPane.showInputDialog(null,"Enter the file name:","New File", JOptionPane.INFORMATION_MESSAGE);
+
+        Document doc = new Document(documentName);
     }
 }
